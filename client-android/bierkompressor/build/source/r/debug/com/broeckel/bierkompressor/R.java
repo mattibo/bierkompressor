2ba7dd67e7e3d30c248d1edeb549cfbc99a526d2
Matti Borchers <matti_borchers@gmx.de>
2013-06-16 21:49:10 +0200

    Menü erstellt (Home Button funktioniert noch nicht), Zufall-Fragment erstellt.

--- client-android/bierkompressor/build/source/r/debug/com/broeckel/bierkompressor/R.java
+++ client-android/bierkompressor/build/source/r/debug/com/broeckel/bierkompressor/R.java
@@ -8,28 +8,46 @@
 package com.broeckel.bierkompressor;
 
 public final class R {
+    public static final class array {
+        public static final int menu_array=0x7f040000;
+    }
     public static final class attr {
     }
     public static final class dimen {
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int drawer_shadow=0x7f020000;
+        public static final int ic_drawer=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int button=0x7f080008;
+        public static final int button_random_player1=0x7f080003;
+        public static final int button_random_player2=0x7f080004;
+        public static final int button_random_player3=0x7f080005;
+        public static final int button_random_player4=0x7f080006;
+        public static final int content_frame=0x7f080001;
+        public static final int drawer_layout=0x7f080000;
+        public static final int left_drawer=0x7f080002;
+        public static final int seekBar=0x7f080007;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-    }
-    public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int drawer_list_item=0x7f030001;
+        public static final int fragment_random=0x7f030002;
     }
     public static final class string {
-        public static final int action_settings=0x7f050000;
-        public static final int app_name=0x7f050001;
-        public static final int hello_world=0x7f050002;
+        public static final int app_name=0x7f060000;
+        public static final int app_name_one=0x7f060001;
+        public static final int drawer_close=0x7f060002;
+        public static final int drawer_open=0x7f060003;
+        public static final int hello_world=0x7f060004;
+        public static final int player_1=0x7f060005;
+        public static final int player_2=0x7f060006;
+        public static final int player_3=0x7f060007;
+        public static final int player_4=0x7f060008;
     }
     public static final class style {
         /** 
@@ -39,9 +57,9 @@ public final class R {
         
  API 11 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
--- client-android/bierkompressor/src/main/java/com/broeckel/bierkompressor/MainActivity.java
+++ client-android/bierkompressor/src/main/java/com/broeckel/bierkompressor/MainActivity.java
@@ -1,13 +1,73 @@
 package com.broeckel.bierkompressor;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.app.Activity;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.widget.DrawerLayout;
+import android.view.Menu;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends Activity {
+
+    private String[] menuItems;
+    private ListView menuDrawerList;
+    private CharSequence menuTitle;
+    private CharSequence fragmentTitle;
+    private DrawerLayout menuDrawerLayout;
+    private ActionBarDrawerToggle menuDrawerToogle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        menuTitle = fragmentTitle = getTitle();
+
+        menuItems = getResources().getStringArray(R.array.menu_array);
+        menuDrawerList = (ListView) findViewById(R.id.left_drawer);
+
+        menuDrawerList.setAdapter(new ArrayAdapter<String>(this, R.layout.drawer_list_item, menuItems));
+
+        menuDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        menuDrawerToogle = new ActionBarDrawerToggle(this, menuDrawerLayout, R.drawable.ic_drawer, R.string.drawer_open, R.string.drawer_close) {
+
+            public void onDrawerClosed(View view) {
+                getActionBar().setTitle(menuTitle);
+                invalidateOptionsMenu();
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                getActionBar().setTitle(fragmentTitle);
+                invalidateOptionsMenu();
+            }
+        };
+
+        menuDrawerLayout.setDrawerListener(menuDrawerToogle);
+
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        getActionBar().setHomeButtonEnabled(true);
+    }
+    
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        boolean drawerOpen = menuDrawerLayout.isDrawerOpen(menuDrawerList);
+        return super.onPrepareOptionsMenu(menu);
+    };
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        menuDrawerToogle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        menuDrawerToogle.onConfigurationChanged(newConfig);
     }
 }
